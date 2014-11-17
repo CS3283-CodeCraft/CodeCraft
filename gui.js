3be4abf4775198a88a096995738c45c521ed5d45
angathorion <tanghuansong@gmail.com>
2014-11-17 21:11:45 +0800

    Improved Sharebox asset sharing for costumes.

--- gui.js
+++ gui.js
@@ -82,6 +82,7 @@ var WardrobeMorph;
 var SoundIconMorph;
 var JukeboxMorph;
 
+var ShareBoxAssetsMorph;
 
 // IDE_Morph ///////////////////////////////////////////////////////////
 
@@ -1723,7 +1724,7 @@ IDE_Morph.prototype.createShareBox = function () {
         // Executes shareBox prototype functionality. To be modified/deleted thereafter
         IDE_Morph.shareBoxPrototypeFunctionality.call(this, myself);
     } else if (this.currentShareBoxTab === 'assets') {
-        this.shareBox = new WardrobeMorph(
+        this.shareBox = new ShareBoxAssetsMorph(
             this.shareBoxPlaceholderSprite,
             this.sliderColor
         );
@@ -7575,4 +7576,112 @@ JukeboxMorph.prototype.reactToDropOf = function (icon) {
     });
     this.sprite.sounds.add(costume, idx);
     this.updateList();
+};
+
+// ShareBoxAssetsMorph ///////////////////////////////////////////////////////
+
+// I am a watcher on the Sharebox for shared costumes
+
+// ShareBoxAssetsMorph inherits from WardrobeMorph
+
+// Huan Song: Some interesting inheritance going here. I still need to figure out what's going on, but this works atm.
+ShareBoxAssetsMorph.prototype = Object.create(WardrobeMorph.prototype);
+
+function ShareBoxAssetsMorph(aSprite, sliderColor) {
+    this.init(aSprite, sliderColor);
+};
+
+
+ShareBoxAssetsMorph.init = function (aSprite, sliderColor) {
+    // additional properties
+    this.sprite = aSprite || new SpriteMorph();
+    this.costumesVersion = null;
+    this.spriteVersion = null;
+
+    // initialize inherited properties
+    ShareBoxAssetsMorph.uber.init.call(this, null, null, sliderColor);
+
+    // configure inherited properties
+    this.fps = 2;
+    this.updateList();
+};
+
+ShareBoxAssetsMorph.prototype.updateList = function () {
+    var myself = this,
+        x = this.left() + 5,
+        y = this.top() + 5,
+        padding = 4,
+        oldFlag = Morph.prototype.trackChanges,
+        oldPos = this.contents.position(),
+        icon,
+        template,
+        txt,
+        paintbutton;
+
+    this.changed();
+    oldFlag = Morph.prototype.trackChanges;
+    Morph.prototype.trackChanges = false;
+
+    this.contents.destroy();
+    this.contents = new FrameMorph(this);
+    this.contents.acceptsDrops = false;
+    this.contents.reactToDropOf = function (icon) {
+        myself.reactToDropOf(icon);
+    };
+    this.addBack(this.contents);
+
+    /*
+    icon = new TurtleIconMorph(this.sprite);
+    icon.setPosition(new Point(x, y));
+    myself.addContents(icon);
+    y = icon.bottom() + padding;
+
+    paintbutton = new PushButtonMorph(
+        this,
+        "paintNew",
+        new SymbolMorph("brush", 15)
+    );
+    paintbutton.padding = 0;
+    paintbutton.corner = 12;
+    paintbutton.color = IDE_Morph.prototype.groupColor;
+    paintbutton.highlightColor = IDE_Morph.prototype.frameColor.darker(50);
+    paintbutton.pressColor = paintbutton.highlightColor;
+    paintbutton.labelMinExtent = new Point(36, 18);
+    paintbutton.labelShadowOffset = new Point(-1, -1);
+    paintbutton.labelShadowColor = paintbutton.highlightColor;
+    paintbutton.labelColor = TurtleIconMorph.prototype.labelColor;
+    paintbutton.contrast = this.buttonContrast;
+    paintbutton.drawNew();
+    paintbutton.hint = "Paint a new costume";
+    paintbutton.setPosition(new Point(x, y));
+    paintbutton.fixLayout();
+    paintbutton.setCenter(icon.center());
+    paintbutton.setLeft(icon.right() + padding * 4);
+
+
+    this.addContents(paintbutton);
+    txt = new TextMorph(localize(
+        "costumes tab help" // look up long string in translator
+    ));
+    txt.fontSize = 9;
+    txt.setColor(SpriteMorph.prototype.paletteTextColor);
+
+    txt.setPosition(new Point(x, y));
+    this.addContents(txt);
+    y = txt.bottom() + padding;
+    */
+    this.sprite.costumes.asArray().forEach(function (costume) {
+        template = icon = new CostumeIconMorph(costume, template);
+        icon.setPosition(new Point(x, y));
+        myself.addContents(icon);
+        x = icon.right() + padding;
+    });
+    this.costumesVersion = this.sprite.costumes.lastChanged;
+
+    this.contents.setPosition(oldPos);
+    this.adjustScrollBars();
+    Morph.prototype.trackChanges = oldFlag;
+    this.changed();
+
+    this.updateSelection();
 };
