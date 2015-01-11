640c577d8b5958fe80066d4d9d4a58ce87dfd8d2
angathorion <tanghuansong@gmail.com>
2015-01-11 21:06:16 +0800

    Fixed unit testing environment. Tests should now work as expected, but more testing (of the tests) is required.
    Signed-off-by: angathorion <tanghuansong@gmail.com>

--- QUnit Tests/tests_sharebox.js
+++ QUnit Tests/tests_sharebox.js
@@ -1,22 +1,20 @@
 /**
  * Created by Tang on 10/1/2015.
  */
+QUnit.module("Environment Setup", {
+    beforeEach: function() {
+        QUnit.stop();
+        this.world = new WorldMorph(document.getElementById('world'));
+        this.world.worldCanvas.focus();
+        new IDE_Morph().openIn(this.world);
+        this.world.doOneCycle();
+        this.ide = this.world.children[0];
+        setTimeout(QUnit.start(), 2000);
+    }
+});
 
 QUnit.test("Serialize empty costume given fresh IDE", function(assert) {
-    var world;
-    var ide;
-    window.onload = function () {
-        world = new WorldMorph(document.getElementById('world'));
-        world.worldCanvas.focus();
-        ide = new IDE_Morph().openIn(world);
-        setInterval(loop, 1);
-    };
-
-    function loop() {
-        world.doOneCycle();
-    }
-
-    var sharer = new ShareBoxItemSharer(new SnapSerializer(), ide);
+    var sharer = new ShareBoxItemSharer(new SnapSerializer(), this.ide);
     assert.equal(sharer.serializeItem(new CostumeIconMorph()),
         "<costume name=\"\" center-x=\"0\" center-y=\"0\" image=\"data:,\" id=\"1\"/>", "Passed!" );
 });
