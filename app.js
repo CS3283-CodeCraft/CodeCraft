700c53a09087681a26b31c8b6afcbb5b4bb8d8da
Zhang Yiwen <yiwen@pigeonlab.com>
2015-04-02 21:11:10 +0800

    add library api

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var shareboxApi = require('./routes/shareboxApi');
+var libraryApi = require('./routes/libraryApi');
+
 
 var app = express();
 
@@ -27,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/api/sharebox', shareboxApi)
+app.use('/api/library', libraryApi)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
